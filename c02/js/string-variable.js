09e399a1ad624bcbdbeb0054c9a43fd6277ab6e0
Jose Romilson <zero.barros@gmail.com>
2017-11-09 14:51:03 -0300

    aula numero 02 pag. 64

---
+++ c02/js/string-variable.js
@@ -0,0 +1,9 @@
+var username;
+var messege;
+username = 'Molly';
+messege = 'See our upcoming range';
+
+var elName = document.getElementById('name');
+elName.textContent = username;
+var elNote = document.getElementById('note');
+elNote.textContent = messege;
