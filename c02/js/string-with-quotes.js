6553fa16084ae30ac835d64bf1e2fd8d706cd2b7
Jose Romilson <zero.barros@gmail.com>
2017-11-09 15:45:36 -0300

    aula numero 02 pag. 65

---
+++ c02/js/string-with-quotes.js
@@ -0,0 +1,11 @@
+var title;
+var messege;
+title = "Moll's Special Offers";
+messege = '<a href = \"sale.html\">25% off!</a>';
+
+var elTitle = document.getElementById('title');
+elTitle.innerHTML = title;
+var elNote = document.getElementById('note');
+elNote.innerHTML = messege;
+
+
